198e3543b103eabc210d8b064eb22ba0781e6bb1
dang minh chau <dangminhchau15@gmail.com>
2023-05-29 16:25:58 +0700

    gitignore

--- app/lib/main_dev.dart
+++ app/lib/main_dev.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'application/config/app_config.dart';
 import 'di/service_locator.dart';
 
-void main() async {
+void main() {
   AppConfig.create(
     appName: "Gomedi Dev",
     baseUrl: Endpoints.devUrl,
     flavor: Flavor.dev,
   );
   WidgetsFlutterBinding.ensureInitialized();
-  await setupLocator(AppConfig.create().baseUrl);
+  // await setupLocator(AppConfig.create().baseUrl);
   runApp(const App());
 }
