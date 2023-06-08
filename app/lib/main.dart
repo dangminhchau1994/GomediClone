46d3c2bbd5eebba8ee5c170da3275d3db9c29127
dang minh chau <dangminhchau15@gmail.com>
2023-06-08 11:36:19 +0700

    change flavor name for ios

---
+++ app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:app/infrastructure/network/api/endpoints.dart';
+import 'package:flutter/material.dart';
+import 'application/app/app_view.dart';
+import 'application/config/app_config.dart';
+import 'di/service_locator.dart';
+
+void main() {
+  final appConfig = AppConfig.create(
+    appName: "Gomedi Prod",
+    baseUrl: Endpoints.prodUrl,
+    flavor: Flavor.dev,
+  );
+  setupLocator(appConfig.baseUrl);
+  runApp(App(appConfig: appConfig));
+}
